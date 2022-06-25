c932277da66e10cf12c01cefb9d1ec0886949e7f
phanlethanhvinh <88189280+phanlethanhvinh@users.noreply.github.com>
2022-06-25 20:57:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'FEEDBACK FOR TAVINY',
+    introDesc: `Hãy đưa ra nhận xét của bạn`,
+    btnIntro: 'Trang tiếp',
+    title: 'Bạn thích các sản phẩm của Taviny chứ?',
+    desc: 'Hmmmm...',
+    btnYes: 'Đúng vậy',
+    btnNo: 'Không',
+    question:'Lí do bạn thích các sản phẩm của Taviny?',
+    btnReply: 'Gửi cho Taviny',
+    reply: 'Vì các sản phẩm của Taviny rất xịn xò kkk',
+    mess: 'Cảm ơn feedback của bạn!',
+    messDesc: 'TavinyⓇ',
+    btnAccept: 'Trở về trang chủ',
+    messLink: 'https://by.com.vn/taviny' 
+}
